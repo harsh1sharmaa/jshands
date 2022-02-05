1a029a0de5f78a1f1714055578370ad6ceec3a1d
cedcoss <harshsharma@cedcoss.com>
2022-02-05 16:58:16 +0530

    Accessing Nested Objects

--- src/index.js
+++ src/index.js
@@ -1,81 +1,15 @@
-// this is comment
-/* This is a
-multi-line comment */
 
-let x=1;
-var y=2;
-console.log(x);
-const a1=3;
-var val;
-
-var myVar;
-myVar = 5;
-console.log(myVar);
- let num=myVar;
-
- let myvalu=10;
- let str="harsh sharma";
-console.log(str);
-
-
-var a=5;
-var b=10;
-var c ="I am a";
-
-
-a = a + 1;
-b = b + 5;
-c = c + " String!";
-
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
-
-
-let catName = "Oliver";
-let catSound = "Meow!";
-
-const FCC = "freeCodeCamp"; 
-let fact = "is cool!"; 
-fact = "is awesome!";
-console.log(FCC, fact); 
-
-const sum = 10 + 10;
-
-const difference = 45 - 33;
-
-const product = 8 * 10;
-let count = 0;
-function fun(val){
-  const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-      "gold": true
+ 
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
     }
-  ];
-  myMusic.push(
-    {
-       "artist": "lly Joel",
-      "title": "Pi Man",
-      "release_year": 193,
-      "formats": [
-        "D",
-        "T",
-        "P"
-      ],
-      "gold": true
+  };
   
-    }
-  )
+  const gloveBoxContents = myStorage.car.inside["glove box"];
