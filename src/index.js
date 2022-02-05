8bd50b18ba4bb05d1785396ccc9eb4de2b379c8a
cedcoss <harshsharma@cedcoss.com>
2022-02-05 12:15:17 +0530

    const myArray = [

--- src/index.js
+++ src/index.js
@@ -53,7 +53,11 @@ const product = 8 * 10;
 
 const quotient = 66 / 33;
 
-const myArray = [18, 64, 99];
-
-
-myArray[0]=45;
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
