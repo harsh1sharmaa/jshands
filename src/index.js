e97161fc7aa61383eb22880baf2be2bc839e23e4
cedcoss <harshsharma@cedcoss.com>
2022-02-05 16:22:07 +0530

    Counting Cards

--- src/index.js
+++ src/index.js
@@ -50,18 +50,22 @@ const sum = 10 + 10;
 const difference = 45 - 33;
 
 const product = 8 * 10;
-// Setup
-function abTest(a, b) {
-  let x;
+let count = 0;
+
+function cc(card) {
   // Only change code below this line
-    if(a<0 || b<0){
-      return x;
-    }
+  let x=" Bet";
+  if(card== 2 || card==3|| card==4|| card==5 ||card==6){
+    count++;
+  }else if(card==10|| card=='J' || card=='Q'|| card=='K' || card=='A'){
+    count--;
+    x=" Hold";
+
+  }
 
 
+  return count+x;
   // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2,2);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
